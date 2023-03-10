f8c6df7a30d88cd3110d3721e6281448e92b763a
Melt <seokwoo2000@naver.com>
2023-03-10 15:38:01 +0900

    프로젝트 생성

--- Algoritms_Cpp/main.cpp
+++ Algoritms_Cpp/main.cpp
@@ -7,8 +7,9 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    std::cout << "브랜치를 확인해주세요\n";
+    
     return 0;
 }
